2a0bf563cd49754454353e94d5c9655a82af7591
Briliyan Sanjaya Hartono <104523406+briliyansanjaya@users.noreply.github.com>
2023-01-24 17:41:30 +0700

    Delete storage directory

--- storage/framework/views/c211a36b98a6212bf08e92237290d932d16bcb55.php
+++
@@ -1,8 +0,0 @@
-<div class="d-flex justify-content-center shadow bg-body-tertiary bg-white sticky-top">
-    <nav class="navbar navbar-light">
-        <a href="/">
-            <img src="<?php echo e(asset('storage/images/bantu_kios-01.png')); ?>" width="300" height="65" alt="Logo">
-        </a>
-    </nav>
-</div>
-<?php /**PATH C:\Users\brili\Downloads\BantuKios\resources\views/components/header.blade.php ENDPATH**/ ?>
