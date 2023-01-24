b457ffd05863da0113c986d8787544fe0846b8b2
Briliyan Sanjaya Hartono <104523406+briliyansanjaya@users.noreply.github.com>
2023-01-24 17:41:11 +0700

    Delete resources directory

--- resources/views/components/header.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="d-flex justify-content-center shadow bg-body-tertiary bg-white sticky-top">
-    <nav class="navbar navbar-light">
-        <a href="/">
-            <img src="{{ asset('storage/images/bantu_kios-01.png') }}" width="300" height="65" alt="Logo">
-        </a>
-    </nav>
-</div>
